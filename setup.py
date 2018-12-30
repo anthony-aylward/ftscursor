1b606eefdfa2cf279b642fc8771c149c8411a844
anthony-aylward <aaylward@eng.ucsd.edu>
2018-12-29 17:22:26 -0800

    quotes in setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ftscursor",
-    version="0.2.0",
-    author="Anthony Aylward",
-    author_email="aaylward@eng.ucsd.edu",
-    description="A sqlite3 cursor with extra methods to support FTS3/4/5",
+    name='ftscursor',
+    version='0.2.0',
+    author='Anthony Aylward',
+    author_email='aaylward@eng.ucsd.edu',
+    description='A sqlite3 cursor with extra methods to support FTS3/4/5',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/anthony-aylward/ftscursor",
+    long_description_content_type='text/markdown',
+    url='https://github.com/anthony-aylward/ftscursor',
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ]
 )
