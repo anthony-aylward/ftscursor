11c658e74980490c0c2a1d8bbac23cf7bc3a4a54
anthony-aylward <aaylward@eng.ucsd.edu>
2018-12-31 08:02:18 -0800

    punctuation

--- ftscursor/__init__.py
+++ ftscursor/__init__.py
@@ -1,6 +1,6 @@
 """This package contains a subclass of the sqlite3.Cursor class that makes
 it easy to perform (simple) full-text-search operations using the sqlite3
-module
+module.
 
 Classes
 -------
